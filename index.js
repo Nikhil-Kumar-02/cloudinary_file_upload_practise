6404455acee3a5b236cfd9545759d7ee599abc15
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-01-03 20:57:11 +0530

    all setup on index.js file done like connect to cloudinary

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
 const connectDB = require('./config/database');
+const cloudinaryConnect = require('./config/cloudinary');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());  
+
+const apiRoutes = require('./routes/fileupload');
+app.use('/api/v1/upload' , apiRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -12,3 +24,4 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
 connectDB();
+cloudinaryConnect();
