608ee93424c5e9af7012ade24866af360c057444
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-01-04 11:58:28 +0530

    earlier i was not able  to open the file now i can open it as now we have  addded extention from the recieved file

--- controller/fileupload.js
+++ controller/fileupload.js
@@ -4,8 +4,10 @@ const localfileUpload = async (req,res) => {
     try {
         const file = req.files.file;
         console.log("the file recieved " , file);
+        //now form the recieved file extract the extention from it which is in name
+        const extention = "." + file.name.split('.')[1];
 
-        const path = __dirname + "/files" + Date.now();
+        const path = __dirname + "/files/" + Date.now() + extention;
         console.log(path);
 
         file.mv(path , (err) => {
